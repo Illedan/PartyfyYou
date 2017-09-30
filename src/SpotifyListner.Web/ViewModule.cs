03ec30487ed6af8be6ac239aca24f0ed8d2d0041
Marius Norøy Lian <lianmarius92@gmail.com>
2017-09-30 15:10:49 +0200

    join and help init

---
+++ src/SpotifyListner.Web/ViewModule.cs
@@ -0,0 +1,25 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifyListner.Web
+{
+    public class ViewModule : NancyModule
+    {
+        public ViewModule()
+        {
+            Get["/help"] = parameters =>
+            {
+                return Response.AsFile("help.html", "text/html");
+            };
+
+            Get["/join"] = parameters =>
+            {
+                return Response.AsFile("join.html", "text/html");
+            };
+        }
+    }
+}
