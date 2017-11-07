92383bb0e596612f6ac8469fd0cd11e45e332a6c
Marius Norøy Lian <lianmarius92@gmail.com>
2017-11-07 20:03:16 +0100

    refreshtoken started

--- src/SpotifyListner.Web/Content/js/videopage.js
+++ src/SpotifyListner.Web/Content/js/videopage.js
@@ -1,4 +1,5 @@
-﻿if (!window.console) console = {};
+﻿'use spotify';
+if (!window.console) console = {};
 console.log = console.log || function () { };
  
 var apiUrlBase = "http://localhost:1337";
@@ -18,8 +19,10 @@ function httpGetRequest(theUrl, callback) {
     xmlHttp.send(null);
 }
 function GetPlayingSong(callback) {
-    console.log(tokenResponse);
     return httpGetRequest(apiUrlBase + '/url?token=' + tokenResponse.access_token, callback);
+}
+function RefreshToken(callback) {
+    return httpGetRequest(apiUrlBase + '/refreshToken?refreshToken=' + tokenResponse.refresh_token, callback);
 }
 function songIdReturned(songId) {
  
@@ -29,6 +32,26 @@ function songIdReturned(songId) {
 		document.getElementById('Myframe').src = newUrl;
 	}  
 }
+
+function tokenReturned(token) {
+
+	var responseString = localStorage.getItem("responseString");
+
+	var storedToken = JSON.parse(responseString);
+
+	var recievedToken = JSON.parse(token);
+
+	var newToken = new Object();
+	newToken.access_token = recievedToken.access_token;
+	newToken.token_type = recievedToken.token_type;
+	newToken.expires_in = recievedToken.expires_in;
+	newToken.refresh_token = storedToken.refresh_token;
+	newToken.scope = recievedToken.scope;
+
+	localStorage.setItem("responseString", JSON.stringify(newToken));
+	localStorage.setItem("timeTicketFetched", Date.now());
+
+}
  
 var tokenResponse;
 var isLoaded = false;
@@ -36,13 +59,20 @@ function loadedFrame() {
     var responseString = localStorage.getItem("responseString");
     tokenResponse = JSON.parse(responseString); // har access_token her
  
-    var timeTicketFetchedString = localStorage.getItem("timeTicketFetched");
+    
+    function loop() {
+        GetPlayingSong(songIdReturned);
+		var timeTicketFetchedString = localStorage.getItem("timeTicketFetched");
     var timeTicketFetched = JSON.parse(timeTicketFetchedString);
     if (timeTicketFetched === null) {
-        localStorage.setItem("timeTicketFetched", Date.now());
+		console.log("timeTicketFetched==null:"+ timeTicketFetched);
+         RefreshToken(tokenReturned);
+
     } else {
         var timeDifference = Date.now() - timeTicketFetched;
+		console.log("timeDifference: " +timeDifference/1000);
         if (timeDifference<60000) {
+		    RefreshToken(tokenReturned);
             //get new token
             //set tokenResponse to the new token
            
@@ -52,8 +82,7 @@ function loadedFrame() {
         }
  
     }
-    function loop() {
-        GetPlayingSong(songIdReturned);
+		
     }
     if (!isLoaded && tokenResponse !== null) {
  
