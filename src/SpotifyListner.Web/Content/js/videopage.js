f2f675ddfbe62ee99b43605a770ab07bb0abdeb6
Marius Norøy Lian <lianmarius92@gmail.com>
2017-11-07 17:38:55 +0100

    Basics works

--- src/SpotifyListner.Web/Content/js/videopage.js
+++ src/SpotifyListner.Web/Content/js/videopage.js
@@ -1,10 +1,12 @@
 ﻿if (!window.console) console = {};
 console.log = console.log || function () { };
-
+ 
+var apiUrlBase = "http://localhost:1337";
+ 
 function createYoutubeUrl(id) {
     return "https://www.youtube.com/embed/" + id + "?autoplay=1";
 }
-
+ 
 function httpGetRequest(theUrl, callback) {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open("GET", theUrl, true); // false for synchronous request
@@ -17,44 +19,66 @@ function httpGetRequest(theUrl, callback) {
 }
 function GetPlayingSong(callback) {
     console.log(tokenResponse);
-    return httpGetRequest("http://localhost:1337" + '/url?token=' + tokenResponse.access_token, callback);
+    return httpGetRequest(apiUrlBase + '/url?token=' + tokenResponse.access_token, callback);
 }
 function songIdReturned(songId) {
+ 
+	var newUrl = createYoutubeUrl(songId);
+	if(document.getElementById('Myframe').src !== newUrl){
 
-   
-    document.getElementById('Myframe').src = createYoutubeUrl(songId);
-
+		document.getElementById('Myframe').src = newUrl;
+	}  
 }
-
+ 
 var tokenResponse;
 var isLoaded = false;
 function loadedFrame() {
-    
-    //localStorage.setItem("token", token);
-    //localStorage.setItem("token_type", token_type);
-    //localStorage.setItem("expires_in", expires_in);
-    if (!isLoaded) {
-
-       
+    var responseString = localStorage.getItem("responseString");
+    tokenResponse = JSON.parse(responseString); // har access_token her
+ 
+    var timeTicketFetchedString = localStorage.getItem("timeTicketFetched");
+    var timeTicketFetched = JSON.parse(timeTicketFetchedString);
+    if (timeTicketFetched === null) {
+        localStorage.setItem("timeTicketFetched", Date.now());
+    } else {
+        var timeDifference = Date.now() - timeTicketFetched;
+        if (timeDifference<60000) {
+            //get new token
+            //set tokenResponse to the new token
+           
+            //set localstorage with new token
+ 
+            //set localstorage with timeTicketFetched
+        }
+ 
+    }
+    function loop() {
+        GetPlayingSong(songIdReturned);
+    }
+    if (!isLoaded && tokenResponse !== null) {
+ 
+      
         isLoaded = true;
         try {
+           
+            setInterval(function () {
+                loop();
+            }, 1000);
+           
             
-            var responseString = localStorage.getItem("responseString");
-            
-            tokenResponse = JSON.parse(responseString); // har access_token her
             
             if (tokenResponse !== null) {
                 GetPlayingSong(songIdReturned);
             } else {
                 console.log("token response from server is null");
             }
-            
+           
             document.getElementById("myspan").innerHTML = code;
         } catch (e) {
             document.getElementById('Myframe').src = createYoutubeUrl("aeWmdojEJf0");
-        } 
-
-        
+        }
+ 
+       
         
     }
 }
