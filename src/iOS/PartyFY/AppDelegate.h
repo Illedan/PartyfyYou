45b234ecada2a1012b5320d297fa9f32a8a25f8d
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2017-11-10 17:33:54 +0100

    Added CocoaPods

---
+++ src/iOS/PartyFY/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PartyFY
+//
+//  Created by Runar Ovesen Hjerpbakk on 10/11/2017.
+//  Copyright © 2017 Runar Ovesen Hjerpbakk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ src/iOS/PartyFY/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PartyFY
+//
+//  Created by Runar Ovesen Hjerpbakk on 10/11/2017.
+//  Copyright © 2017 Runar Ovesen Hjerpbakk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
