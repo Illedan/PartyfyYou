da4440ef01caaa72967c69c8fb6a96b0ecfbf2f8
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2017-11-14 20:17:33 +0100

    App skeleton

---
+++ src/tvOS/PartifyTV/PartifyTV/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PartifyTV
+//
+//  Created by Runar Ovesen Hjerpbakk on 14/11/2017.
+//  Copyright © 2017 Runar Ovesen Hjerpbakk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ src/tvOS/PartifyTV/PartifyTV/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PartifyTV
+//
+//  Created by Runar Ovesen Hjerpbakk on 14/11/2017.
+//  Copyright © 2017 Runar Ovesen Hjerpbakk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
