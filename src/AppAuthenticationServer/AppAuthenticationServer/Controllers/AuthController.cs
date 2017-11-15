b6413a49b7c857ff38b94b1b888afbfc98adb94e
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2017-11-15 09:35:01 +0100

    Added auth service

---
+++ src/AppAuthenticationServer/AppAuthenticationServer/Controllers/AuthController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using AppAuthenticationServer.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace AppAuthenticationServer.Controllers
+{
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        AuthService authService;
+
+        public AuthController(AuthService authService) => this.authService = authService;
+
+        // GET api/auth/f19a5452-9e3a-4768-84b7-a34aaa07c4bc 
+        [HttpGet("{userId}")]
+        public string Get(string userId)
+        {
+            // TODO: Check if user is authenticated (in cache)
+            // TODO: Clear cache
+            // TODO: Return token of some sort
+
+            // Questions
+            // TODO: Do all auth through this? 
+            // TODO: Security? User certificate validation?
+            // TODO: How to refresh on apple tv? 
+
+            if (string.IsNullOrEmpty(userId))
+            {
+                return "";
+            }
+
+            return authService.GetTokenForUserId(userId);
+        }
+
+        // PUT api/auth/f19a5452-9e3a-4768-84b7-a34aaa07c4bc 
+        [HttpPut("{userId}")]
+        public void Put(string userId, [FromBody]string token)
+        {
+            // TODO: Kall denne når autentisert slik at tvapp kan hente token, men når er det?
+            if (string.IsNullOrEmpty(userId))
+            {
+                return;
+            }
+
+            if (string.IsNullOrEmpty(token)) {
+                return;
+            }
+
+            authService.SetTokenForUserId(userId, token);
+        }
+    }
+}
---
+++ src/AppAuthenticationServer/AppAuthenticationServer/Services/AuthService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace AppAuthenticationServer.Services
+{
+    public class AuthService
+    {
+        readonly IMemoryCache memoryCache;
+        readonly TimeSpan tokenTimeout;
+
+        public AuthService(IMemoryCache memoryCache) {
+            this.memoryCache = memoryCache;
+            tokenTimeout = TimeSpan.FromMinutes(5);
+        } 
+
+        public string GetTokenForUserId(string userId) {
+            if (memoryCache.TryGetValue(userId, out string token)) {
+                memoryCache.Remove(userId);
+                return token;
+            }
+
+            return "";
+        }
+            
+        // TODO: Må ha noe token greier som bruker ser på skjermen som han kan skrive inn på websiden.
+        // TODO: Ellers må bruker ha iOS appen slik at den kan la bruker trykke OK elno
+        public void SetTokenForUserId(string userId, string token) {
+            memoryCache.Remove(userId);
+            var cacheEntryOptions = new MemoryCacheEntryOptions()
+                .SetAbsoluteExpiration(tokenTimeout);
+            memoryCache.Set(userId, token, cacheEntryOptions);
+        }
+    }
+}
